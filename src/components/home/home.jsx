661967da3620b378cef43c3c0f1901c0eabde649
Priyatharsini Sivalingam <112175724+Priya-Sivalingam@users.noreply.github.com>
2024-07-03 18:36:45 +0530

    updates

---
+++ src/components/home/home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './home.css'
+import CTA from './CTA'
+import ME  from '../../assets/WhatsApp.jpg'
+
+
+const header = () => {
+  return (
+    
+    <section id='home'>
+    <header>
+     <div className='container header_container '>
+      <h5>Hello I'm</h5>
+      <h1>Priyatharsini Sivalingam</h1>
+      <h5 className='.container'>Computer Engineering Student</h5>
+      <CTA/>
+      <div className='me'>
+        <img src={ME} alt='me'/>
+      </div>
+
+     </div>
+
+
+    </header>
+    
+    </section>
+  )
+}
+
+export default header
